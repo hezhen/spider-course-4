028987d2c37106c4b05a0077f20be79ab068937d
hezhen <hezhen112058@pwrd.com>
2018-06-09 19:17:21 +0800

    add baike

---
+++ baidu/baike.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import urllib3
+import urllib
+from urllib.parse import quote
+import string
+
+url_format = 'https://baike.baidu.com/item/'
+
+item_name = '阿尔伯特·爱因斯坦'
+
+url = url_format + item_name
+
+url = quote(url,safe=string.printable)
+
+http = urllib3.PoolManager()
+r = http.request('GET', url)
+f = open( item_name + '.html', 'wb+')
+f.write(r.data)
+f.close()
